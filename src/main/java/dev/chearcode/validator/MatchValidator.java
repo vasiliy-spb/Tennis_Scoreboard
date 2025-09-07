1fdccbcb8becb47d3dd58f244d335bfe0a15ab14
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-09-07 07:34:42 +0700

    Refactor of MatchValidator

--- src/main/java/dev/chearcode/validator/MatchValidator.java
+++ src/main/java/dev/chearcode/validator/MatchValidator.java
@@ -5,7 +5,6 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class MatchValidator implements ConstraintValidator<ValidMatch, Object> {
     private static final int MAX_NAME_LENGTH = 120;
@@ -18,50 +17,63 @@ public class MatchValidator implements ConstraintValidator<ValidMatch, Object> {
             String firstPlayerName = createMatchRequestDto.firstPlayerName().trim();
             String secondPlayerName = createMatchRequestDto.secondPlayerName().trim();
 
-            if (!isCorrectName(context, firstPlayerName)) {
+            if (!isCorrectName(firstPlayerName, context)) {
                 return false;
             }
 
-            if (!isCorrectName(context, secondPlayerName)) {
+            if (!isCorrectName(secondPlayerName, context)) {
                 return false;
             }
 
-            return isDifferentName(firstPlayerName, secondPlayerName);
+            return isDifferentName(firstPlayerName, secondPlayerName, context);
         }
 
         return false;
     }
 
-    private boolean isDifferentName(String firstPlayerName, String secondPlayerName) {
-        return !firstPlayerName.equalsIgnoreCase(secondPlayerName);
-    }
-
-    private boolean isCorrectName(ConstraintValidatorContext context, String name) {
-        if (name == null || name.isBlank()) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("name must not be blank")
-                    .addConstraintViolation();
-            return false;
-        }
-
-        int partLength = Arrays.stream(name.split("\s+"))
-                .map(String::length)
-                .max(Comparator.naturalOrder())
-                .get();
-
-        if (partLength > MAX_NAME_PART_LENGTH) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("name part length must be less then " + MAX_NAME_PART_LENGTH)
-                    .addConstraintViolation();
-            return false;
-        }
-
-        if (name.length() > MAX_NAME_LENGTH) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("name length must be less then " + MAX_NAME_LENGTH)
-                    .addConstraintViolation();
+    private boolean isDifferentName(String firstPlayerName, String secondPlayerName, ConstraintValidatorContext context) {
+        if (firstPlayerName.equalsIgnoreCase(secondPlayerName)) {
+            setMessage(context, "names must be different");
             return false;
         }
         return true;
+    }
+
+    private void setMessage(ConstraintValidatorContext context, String message) {
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(message)
+                .addConstraintViolation();
+    }
+
+    private boolean isCorrectName(String name, ConstraintValidatorContext context) {
+        if (isBlank(name)) {
+            setMessage(context, "name must not be blank");
+            return false;
+        }
+
+        if (!hasCorrectLength(name)) {
+            setMessage(context, "name length must be less then " + MAX_NAME_LENGTH);
+            return false;
+        }
+
+        if (!hasCorrectPartsLength(name)) {
+            setMessage(context, "name part length must be less then " + MAX_NAME_PART_LENGTH);
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isBlank(String name) {
+        return name == null || name.isBlank();
+    }
+
+    private boolean hasCorrectLength(String name) {
+        return name.length() <= MAX_NAME_LENGTH;
+    }
+
+    private boolean hasCorrectPartsLength(String name) {
+        return Arrays.stream(name.split("\s+"))
+                .map(String::length)
+                .allMatch(l -> l <= MAX_NAME_PART_LENGTH);
     }
 }
