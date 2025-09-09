ca79f3a2cb6442b20b4def63a2700e13b42cecc9
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-09-10 00:33:48 +0700

    Refactor of validation

--- src/main/java/dev/chearcode/validation/UuidValidator.java
+++ src/main/java/dev/chearcode/validation/UuidValidator.java
@@ -5,18 +5,22 @@ import dev.chearcode.exception.ValidationException;
 import java.util.UUID;
 
 public final class UuidValidator {
+
+    private static final String BLANK_MESSAGE = "UUID must not be blank";
+    private static final String INCORRECT_FORMAT_MESSAGE = "incorrect format UUID";
+
     private UuidValidator() {
     }
 
     public static void validate(String uuid) {
         if (uuid == null || uuid.isBlank()) {
-            throw new ValidationException("UUID must not be blank");
+            throw new ValidationException(BLANK_MESSAGE);
         }
 
         try {
             UUID.fromString(uuid);
         } catch (IllegalArgumentException e) {
-            throw new ValidationException("incorrect format UUID");
+            throw new ValidationException(INCORRECT_FORMAT_MESSAGE);
         }
     }
 }
