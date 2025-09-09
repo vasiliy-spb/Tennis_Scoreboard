35559a6e74be9d977b6f0f6bd7171925641f66aa
vasiliy-spb <vasiliy-spb07@mail.ru>
2025-09-10 01:13:17 +0700

    Remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,26 +5,26 @@ plugins {
 }
 
 group = "dev.chearcode"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
+    implementation("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.0")
+    implementation("org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1")
+
     implementation("org.hibernate.validator:hibernate-validator:9.0.1.Final")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("org.hibernate.orm:hibernate-core:7.1.0.Final")
     implementation("com.h2database:h2:2.3.232")
     implementation("com.zaxxer:HikariCP:7.0.2")
 
-    implementation("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.0")
-    implementation("org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1")
-
     runtimeOnly("org.slf4j:slf4j-simple:2.0.7")
+
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
